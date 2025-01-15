0cf4ed6b73a034199366ba37c7b749cb68e3d3d5
fai247012575 <247012575@stu.vtc.edu.hk>
2025-01-15 18:57:52 +0800

    upload3
    move2

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,7 +9,25 @@ public class PlayerController : MonoBehaviour
 
     private float moveSpeed = 1;
 
+    [SerializeField]
+
+    private float lookSenesitivity = 5;
+
+    [SerializeField]
+
+    private float jumpHeight = 10;
+
+    [SerializeField]
+
+    private float gravity = 9.81f;
+
     private Vector2 moveVector;
+
+    private Vector2 lookVector;
+
+    private Vector3 rotation;
+
+    private float verticalVelocity;
 
     private CharacterController characterController;
     // Start is called before the first frame update
@@ -22,6 +40,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+        Rotate();
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -31,7 +50,38 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        Vector3 move = transform.right*moveVector.x + transform.forward*moveVector.y;
+        verticalVelocity += -gravity*Time.deltaTime;
+
+        if(characterController.isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -0.1f*gravity*Time.deltaTime;
+        }
+
+        Vector3 move = transform.right*moveVector.x + transform.forward*moveVector.y + transform.up*verticalVelocity;
         characterController.Move(move*moveSpeed*Time.deltaTime);
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookVector = context.ReadValue<Vector2>();
+    }
+
+    private void Rotate()
+    {
+        rotation.y += lookVector.x*lookSenesitivity*Time.deltaTime;
+        transform.localEulerAngles = rotation;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(characterController.isGrounded && context.performed)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        verticalVelocity = Mathf.Sqrt(jumpHeight*gravity);
     }
 }
